6f38cb164bafbe23a5ceed060874798b5b4ff690
Sujal Shrestha <suj9763@gmail.com>
2020-04-04 12:30:14 +0545

    Initial Commit

---
+++ MovieBooking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieBooking
+//
+//  Created by Sujal on 4/4/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
